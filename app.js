924a21bdc78e3367d7eb007cdfdc09890dd9f244
Jim <jadavey@hotmail.com>
2021-02-03 16:37:06 +0000

    document.querySelectors
    html and querySelectors set up

---
+++ app.js
@@ -0,0 +1,12 @@
+// const keyHit = document.querySelector("#inputkey"); not needed
+
+const header = document.querySelector("h1");
+const eventKey = document.querySelector("#eventkey");
+const eventCode = document.querySelector("#eventcode");
+const eventWhich = document.querySelector("#eventwhich");
+
+document.addEventListener("keydown", () => {
+    console.log("intereseting");
+    
+
+})
